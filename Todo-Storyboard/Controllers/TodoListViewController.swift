1c83c23963d56b9b51d0c8928c67cbfd40aeba3a
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-03-09 17:19:31 +0400

    + Add new item to the list using alert

--- Todo-Storyboard/Controllers/TodoListViewController.swift
+++ Todo-Storyboard/Controllers/TodoListViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class TodoListViewController: UITableViewController  {
     
-    let itemArray = ["Find Sugar", "Buy Eggs", "Dribble NBA2k"]
+    var itemArray = ["Find Sugar", "Buy Eggs", "Dribble NBA2k"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,8 +28,46 @@ class TodoListViewController: UITableViewController  {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
         cell.textLabel?.text = itemArray[indexPath.row]
-        
+
         return cell
     }
+    
+    //MARK: --> TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(itemArray[indexPath.row])
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: false)
+    }
+    
+    
+    //MARK: --> Add New Items
+    @IBAction func addButtonTapped(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        let action = UIAlertAction(title: "Add Item", style: .default) { action in
+            self.itemArray.append(textField.text ?? "Placeholder")
+            self.tableView.reloadData()
+        }
+        
+        alert.addAction(action)
+        alert.addAction(cancel)
+        alert.addTextField { alertTextField in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
 
