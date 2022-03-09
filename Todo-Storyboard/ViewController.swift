880d015e6527d279242021e0ed8fe91eddb487c7
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-03-09 15:36:19 +0400

    first commit

---
+++ Todo-Storyboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Todo-Storyboard
+//
+//  Created by Irakli Sokhaneishvili on 09.03.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
