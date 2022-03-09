98579ca8d3ea77e4bd925dbecd7fd4af21d3d1b0
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-03-09 18:41:22 +0400

    + Custom model and refactoring

--- Todo-Storyboard/Controllers/TodoListViewController.swift
+++ Todo-Storyboard/Controllers/TodoListViewController.swift
@@ -11,14 +11,26 @@ class TodoListViewController: UITableViewController  {
     
     let defaults = UserDefaults.standard
     
-    var itemArray = ["Find Sugar", "Buy Eggs", "Dribble NBA2k"]
+    var itemsArray = [Item]()
+    
+//    var itemsArray = ["Find Sugar", "Buy Eggs", "Dribble NBA2k", "Lorem ipsum", "dolor sit amet", "consectetur adipiscing", "sed do eiusmod", "tempor incididunt ut", "labore et dolore magna", "Ut enim ad minim", "quis nostrud exercitation", "ullamco laboris nisi", "aliquip ex ea commodo", "Lorem ipsum", "dolor sit amet", "consectetur adipiscing", "sed do eiusmod", "tempor incididunt ut", "ullamco laboris nisi", "aliquip ex ea commodo", "Lorem ipsum", "anim id est laborum", "aliquip ex ea commodo", "Lorem ipsum", "dolor sit amet", "consectetur adipiscing", "sed do eiusmod", "tempor incididunt ut", "ullamco laboris nisi", "aliquip ex ea commodo", "Lorem ipsum", "anim id est laborum"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
-            itemArray = items
-        }
+        var newItem = Item()
+        newItem.title = "Find Sugar"
+        var newItem2 = Item()
+        newItem2.title = "Buy Eggs"
+        var newItem3 = Item()
+        newItem3.title = "Lorem ipsum"
+        itemsArray.append(newItem)
+        itemsArray.append(newItem2)
+        itemsArray.append(newItem3)
+        
+//        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+//            itemsArray = items
+//        }
         
         title = "Todoey"
         navigationController?.navigationBar.prefersLargeTitles = true
@@ -27,13 +39,15 @@ class TodoListViewController: UITableViewController  {
     //MARK: --> TableView Datasource Methods
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return itemArray.count
+        return itemsArray.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        let item = itemsArray[indexPath.row]
+        cell.textLabel?.text = item.title
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        cell.accessoryType = item.status ? .checkmark : .none
 
         return cell
     }
@@ -41,15 +55,11 @@ class TodoListViewController: UITableViewController  {
     //MARK: --> TableView Delegate Methods
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print(itemArray[indexPath.row])
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        itemsArray[indexPath.row].status.toggle()
         
         tableView.deselectRow(at: indexPath, animated: false)
+        tableView.reloadData()
     }
     
     
@@ -61,8 +71,10 @@ class TodoListViewController: UITableViewController  {
         let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
         let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
         let action = UIAlertAction(title: "Add Item", style: .default) { action in
-            self.itemArray.append(textField.text ?? "Placeholder")
-            self.defaults.set(self.itemArray, forKey: "TodoListArray")
+            var newItem = Item()
+            newItem.title = textField.text!
+            self.itemsArray.append(newItem)
+            self.defaults.set(self.itemsArray, forKey: "TodoListArray")
             self.tableView.reloadData()
         }
         
---
+++ Todo-Storyboard/Models/Item.swift
@@ -0,0 +1,13 @@
+//
+//  Item.swift
+//  Todo-Storyboard
+//
+//  Created by Irakli Sokhaneishvili on 09.03.22.
+//
+
+import Foundation
+
+struct Item {
+    var title: String = ""
+    var status: Bool = false
+}
