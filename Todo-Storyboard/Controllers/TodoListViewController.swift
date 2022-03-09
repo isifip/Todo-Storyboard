ca05014a347dec3fee8ada4b9377352710069767
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-03-09 16:10:35 +0400

    + Basic TableView

--- Todo-Storyboard/Controllers/TodoListViewController.swift
+++ Todo-Storyboard/Controllers/TodoListViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class TodoListViewController: UITableViewController  {
+    
+    let itemArray = ["Find Sugar", "Buy Eggs", "Dribble NBA2k"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "Todoey"
+        navigationController?.navigationBar.prefersLargeTitles = true
     }
-
-
+    
+    //MARK: --> TableView Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
 }
 
