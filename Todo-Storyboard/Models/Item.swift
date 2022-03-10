e5d01118f312ce6e6cdf6b7f47b595291c42318e
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-03-10 18:56:03 +0400

    + Core data configuration

--- Todo-Storyboard/Models/Item.swift
+++ Todo-Storyboard/Models/Item.swift
@@ -5,9 +5,9 @@
 //  Created by Irakli Sokhaneishvili on 09.03.22.
 //
 
-import Foundation
-
-struct Item: Codable {
-    var title: String = ""
-    var status: Bool = false
-}
+//import Foundation
+//
+//struct Item: Codable {
+//    var title: String = ""
+//    var status: Bool = false
+//}
