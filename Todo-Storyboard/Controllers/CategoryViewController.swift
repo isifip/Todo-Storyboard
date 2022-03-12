82a272afcba1821634f0858a1ad46f1b5b1882ed
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-03-12 15:43:23 +0400

    + Setup CategoryViewController. DataSource methods, Data Manipulation Methods and etc..

--- Todo-Storyboard/Controllers/CategoryViewController.swift
+++ Todo-Storyboard/Controllers/CategoryViewController.swift
@@ -6,19 +6,85 @@
 //
 
 import UIKit
+import CoreData
 
 class CategoryViewController: UITableViewController {
 
+    var categories = [Category]()
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = "Todoey"
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        loadCategory()
 
     }
     
+    //MARK: --> TableView DataSource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        categories.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        let category = categories[indexPath.row]
+        
+        cell.textLabel?.text = category.name
+        
+        return cell
+    }
+    
+    
+    //MARK: --> TableView Delegate Methods
+    
+    
+    //MARK: --> Add New Categories
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add New Category", message: "", preferredStyle: .alert)
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        let action = UIAlertAction(title: "Add Category", style: .default) { action in
+            let newCategory = Category(context: self.context)
+            newCategory.name = textField.text!
+           
+            self.categories.append(newCategory)
+            
+            self.saveCategory()
+        }
+
+        alert.addAction(action)
+        alert.addAction(cancel)
+        alert.addTextField { alertTextField in
+            alertTextField.placeholder = "Create new category"
+            textField = alertTextField
+        }
+        
+        present(alert, animated: true, completion: nil)
         
     }
     
+    //MARK: --> Data Manipulation Methods
+    
+    func saveCategory() {
+        do {
+            try context.save()
+        } catch {
+            print("Error saving context, \(error)")
+        }
+        
+        tableView.reloadData()
+    }
+    
+    func loadCategory(with request: NSFetchRequest<Category> = Category.fetchRequest()) {
+        do {
+            categories = try context.fetch(request)
+        } catch {
+            print("Error fetching data from context: \(error)")
+        }
+        tableView.reloadData()
+    }
 }
