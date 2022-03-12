598cd7e7a11aab6ef2f1909d2693ad0c4bc208eb
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-03-12 14:38:24 +0400

    + Getting to original state after tapping x button on search bar

--- Todo-Storyboard/Controllers/TodoListViewController.swift
+++ Todo-Storyboard/Controllers/TodoListViewController.swift
@@ -112,5 +112,15 @@ extension TodoListViewController: UISearchBarDelegate {
         
         loadItems(with: request)
     }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            loadItems()
+            
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+        }
+    }
 }
 
